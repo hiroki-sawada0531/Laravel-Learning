091cf7992e69fb63e49e71d9f0109d3c6989483b
澤田大樹 <hiroki.bigtree@icloud.com>
2020-09-09 00:26:16 +0900

    index.phpの表示

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index');
+Route::get('hello', function() {
+    return view('hello.index');
+});

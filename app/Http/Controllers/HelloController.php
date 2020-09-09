fe24d92af8d717adb15e62b10eb65d564ada51cb
澤田大樹 <hiroki.bigtree@icloud.com>
2020-09-09 22:18:52 +0900

    ＠ifディレクティブ、elseエラーで失敗 syntax error, unexpected 'else' (T_ELSE), expecting end of file (View: /Users/sawadahiroki/TEST1/resources/views/hello/index.blade.php

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,23 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-class HelloController extends Controller{
+class HelloController extends Controller
+{
+
 
     public function index()
     {
-        $data = [
-            'msg'=>'お名前を入力して下さい。',
-        ];
-        return view('hello.index', $data);
+        return view('hello.index', ['msg'=>'']);
     }
 
 
     public function post(Request $request)
     {
-        $msg = $request->msg;
-        $data = [
-            'msg'=>'こんにちは、' . $msg . 'さん！',
-        ];
-        return view('hello.index', $data);
+        return view('hello.index', ['msg'=>$request->msg]);
     }
+
 }
--- resources/views/Hello/index.php
+++ resources/views/Hello/index.php
@@ -8,8 +8,5 @@
     </style>
 </head>
 <body>
-        <h1>Index</h1>
-        <p><?php echo $msg; ?></p>
-        <p><?php echo $id; ?></p>
 </body>
 </html>

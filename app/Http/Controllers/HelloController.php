b03285ac9fd2f841af15fc5df5062b87896c31e3
澤田大樹 <hiroki.bigtree@icloud.com>
2020-09-12 20:57:51 +0900

    item.blade.phpファイルの作成。@eachによる繰り返し表示

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -1,47 +1,23 @@
 <?php
 
- 
-
 namespace App\Http\Controllers;
-
- 
 
 use Illuminate\Http\Request;
 
-use Illuminate\Http\Response;
-
- 
-
 class HelloController extends Controller
-
 {
 
- 
-
- 
-
     public function index()
-
     {
 
-        $data = ['one', 'two', 'three', 'four', 'five'];
+        $data = [
+            ['name'=>'山田たろう', 'mail'=>'taro@yamada'],
+            ['name'=>'田中はなこ', 'mail'=>'hanako@flower'],
+            ['name'=>'鈴木さちこ', 'mail'=>'sachico@happy']
+        ];
 
-        return view('hello.index', ['data'=>$data]);
-
-    }
-
- 
-
- 
-
-    public function post(Request $request)
-
-    {
-
-        return view('hello.index', ['msg'=>$request->msg]);
+        return view('hello.index', ['data' => $data]);
 
     }
-
- 
 
 }

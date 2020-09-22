d11f1b3c70d77fb242e5ed311739e384220300de
澤田大樹 <hiroki.bigtree@icloud.com>
2020-09-23 00:41:53 +0900

    バリデーションルールの作成と使用

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -11,14 +11,9 @@ class HelloServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $validator = $this->app['validator'];
-        $validator->resolver(function($translator, $data,
-                    $rules, $messages) {
-                return new HelloValidator($translator, $data,
-                    $rules, $messages);
-                    });
-        View::composer(
-            'hello.index','App\Http\Composers\HelloComposer'
-        );
+        Validator::extend('hello', function($attribute, $value,
+                $parameters, $validator) {
+            return $value % 2 == 0;
+                });
     }
 }

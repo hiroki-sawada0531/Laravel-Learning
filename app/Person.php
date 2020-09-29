ee1b2735138156c00b8b84b8a4856b83b7da575e
澤田大樹 <hiroki.bigtree@icloud.com>
2020-09-30 00:25:20 +0900

    スコープ用メソッドを追加

--- app/Person.php
+++ app/Person.php
@@ -15,4 +15,14 @@ class Person extends Model
     {
         return $query->where('name', $str);
     }
+
+    public function scopeAgeGreaterThan($quary, $n)
+    {
+        return $quary->where('age','>=', $n);
+    }
+
+    public function scopeAgeLessThan($quary, $n)
+    {
+        return $quary->where('age', '<=', $n);
+    }
 }

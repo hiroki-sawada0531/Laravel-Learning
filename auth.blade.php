fc6f6fd56f1016fc53dac69bc6a4e4210f21676c
澤田大樹 <hiroki.bigtree@icloud.com>
2020-10-04 00:23:26 +0900

    修正

--- auth.blade.php
+++ auth.blade.php
@@ -12,12 +12,18 @@
     <form action="/hello/auth" method="post">
       <table>
         @csrf
-        <tr><th>mail: </th><td><input type="text"
-            name="email"></td></tr>
-        <tr><th>pass: </th><td><input type="password"
-            name="password"></td></tr>
-        <tr><th></th><td><input type="submit"
-            name="send"></td></tr>
+        <tr>
+          <th>mail: </th>
+          <td><input type="text" name="email"></td>
+        </tr>
+        <tr>
+          <th>pass: </th>
+          <td><input type="password" name="password"></td>
+        </tr>
+        <tr>
+          <th></th>
+          <td><input type="submit" name="send"></td>
+        </tr>
       </table>
     </form>
 @endsection

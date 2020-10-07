3e488f161aecc9cb7945127cf3719e1ab899aeef
澤田大樹 <hiroki.bigtree@icloud.com>
2020-10-07 22:01:08 +0900

    ログイン処理を実行

--- routes/web.php
+++ routes/web.php
@@ -48,5 +48,6 @@ Route::post('hello/session', 'HelloController@ses_put');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('hello', 'HelloController@index')->middleware('auth');
 Route::get('hello/auth', 'HelloController@getAuth');
 Route::post('hello/auth', 'HelloController@postAuth');

1c04a3f3b02baae45bbca10352a38aa887c67c22
澤田大樹 <hiroki.bigtree@icloud.com>
2020-10-07 23:27:11 +0900

    修正

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,29 +4,13 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-use App\User;
 
-class HelloTest extends TestCase
+class ExampleTest extends TestCase
 {
-
-    use DatabaseMigrations;
-
-    public function testHello()
+    public function testBasicTest()
     {
-        $this->assertTrue(true);
-
         $response = $this->get('/');
+
         $response->assertStatus(200);
-
-        $response = $this->get('/hello');
-        $response->assertStatus(302);
-
-        $user = factory(User::class)->create();
-        $response = $this->actingAs($user)->get('/hello');
-        $response->assertStatus(200);
-
-        $response = $this->get('/no_route');
-        $response->assertStatus(404);
     }
 }
-
